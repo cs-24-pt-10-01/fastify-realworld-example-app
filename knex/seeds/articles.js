0982500a44a70d8a29bf8dd26fa4722dadc02b02
Jakob <31919330+Jakobzs@users.noreply.github.com>
2024-04-16 11:14:37 +0200

    remove instrument for knex

--- knex/seeds/articles.js
+++ knex/seeds/articles.js
@@ -1,30 +1,24 @@
-const rapl = require('./rapl.js');
-rapl.start("1:articles.js:require");
-const {faker} = require('@faker-js/faker');
-rapl.stop("1:articles.js:require");
-rapl.start("7:articles.js:insert");
+const { faker } = require('@faker-js/faker')
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
 exports.seed = async function (knex) {
-  rapl.start("9:articles.js:del");
-  await knex('articles').del();
-  rapl.stop("9:articles.js:del");
-  rapl.start("10:articles.js:insert");
-  await knex('articles').insert(Array.from({
-    length: 5
-  }, (v, i) => {
-    rapl.start("12:articles.js:toISOString");
-    const __result = {
-      id: i,
-      slug: faker.lorem.slug(),
-      title: faker.lorem.sentence(),
-      description: faker.lorem.sentences(),
-      body: faker.lorem.paragraphs(),
-      created_at: new Date().toISOString(),
-      updated_at: new Date().toISOString(),
-      author: i
-    };
-    rapl.stop("12:articles.js:toISOString");
-    return __result;
-  }));
-  rapl.stop("10:articles.js:insert");
-};
-rapl.stop("7:articles.js:insert");
+  // Deletes ALL existing entries
+  await knex('articles').del()
+  await knex('articles').insert(
+    Array.from({ length: 5 }, (v, i) => {
+      return {
+        id: i,
+        slug: faker.lorem.slug(),
+        title: faker.lorem.sentence(),
+        description: faker.lorem.sentences(),
+        body: faker.lorem.paragraphs(),
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+        author: i
+      }
+    })
+  )
+}
--- knex/seeds/users.js
+++ knex/seeds/users.js
@@ -1,33 +1,25 @@
-const rapl = require('./rapl.js');
-rapl.start("1:users.js:require");
-const bcrypt = require('bcrypt');
-rapl.stop("1:users.js:require");
-rapl.start("2:users.js:require");
-const {faker} = require('@faker-js/faker');
-rapl.stop("2:users.js:require");
-rapl.start("8:users.js:insert");
+const bcrypt = require('bcrypt')
+const { faker } = require('@faker-js/faker')
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
 exports.seed = async function (knex) {
-  rapl.start("10:users.js:del");
-  await knex('users').del();
-  rapl.stop("10:users.js:del");
-  rapl.start("11:users.js:insert");
-  await knex('users').insert(Array.from({
-    length: 5
-  }, (v, i) => {
-    rapl.start("13:users.js:toISOString");
-    const __result = {
-      id: i,
-      email: `user${i}@demo.test`,
-      username: faker.name.firstName(),
-      password: bcrypt.hashSync(`user${i}pass`, 10),
-      bio: faker.lorem.sentences(),
-      image: faker.image.avatar(),
-      created_at: new Date().toISOString(),
-      updated_at: new Date().toISOString()
-    };
-    rapl.stop("13:users.js:toISOString");
-    return __result;
-  }));
-  rapl.stop("11:users.js:insert");
-};
-rapl.stop("8:users.js:insert");
+  // Deletes ALL existing entries
+  await knex('users').del()
+  await knex('users').insert(
+    Array.from({ length: 5 }, (v, i) => {
+      return {
+        id: i,
+        email: `user${i}@demo.test`,
+        username: faker.name.firstName(),
+        password: bcrypt.hashSync(`user${i}pass`, 10),
+        bio: faker.lorem.sentences(),
+        image: faker.image.avatar(),
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString()
+      }
+    })
+  )
+}
