908a94e0a1ecf1e15f934b100ffac226e47ceaf2
Villiam <Vjacob19@student.aau.dk>
2024-05-31 14:03:28 +0200

    new run script

---
+++ run.sh
@@ -0,0 +1,19 @@
+# full
+cd ./Orginal
+bash ./run.sh
+cd ..
+
+# compare
+cd ./Stubbed_Compare
+bash ./run.sh
+cd ..
+
+# GetArticles
+cd ./Stubbed_GetArticles
+bash ./run.sh
+cd ..
+
+# Hash
+cd ./Stubbed_Hash
+bash ./run.sh
+cd ..
