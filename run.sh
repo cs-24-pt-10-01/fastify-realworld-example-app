eb8248f81ae871d5032350ccdbfcc10fe58224f3
Villiam <Vjacob19@student.aau.dk>
2024-06-03 10:30:42 +0200

    Adding sleeps

--- run.sh
+++ run.sh
@@ -3,17 +3,25 @@ cd ./Original
 bash ./run.sh
 cd ..
 
+sleep 5s
+
 # compare
 cd ./Stubbed_Compare
 bash ./run.sh
 cd ..
+
+sleep 5s
 
 # GetArticles
 cd ./Stubbed_GetArticles
 bash ./run.sh
 cd ..
 
+sleep 5s
+
 # Hash
 cd ./Stubbed_Hash
 bash ./run.sh
 cd ..
+
+sleep 5s
